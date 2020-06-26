9377606ec6cc4c0f1eb202452db61b5ff08f107b
Or Levi <orlevi128@gmail.com>
2020-06-27 02:01:28 +0300

    removed gunicorn from requirements.txt

--- run_service.sh
+++ run_service.sh
@@ -5,4 +5,5 @@ python download_models.py
 python main_mest.py
 
 ## You can also run the service on gunicorn:
+## RUN: pip install gunicorn[gevent]
 # gunicorn -b 0.0.0.0:5000 -k gevent -w 4 main_mest:app
